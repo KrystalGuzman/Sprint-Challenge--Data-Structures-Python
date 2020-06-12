ed44d771eaecd6e4a23319fd96fbb3fc21726098
Krystal Guzman <kryguzman@gmail.com>
2020-06-12 10:08:26 -0700

    adds stretch to names

--- names/names.py
+++ names/names.py
@@ -22,23 +22,39 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # use Binary Search Tree to go through and search duplicates using contains
 # Runtime ~.25, Runtime complexity O(log(n))
-a = BSTNode("")
-bst = BSTNode(names_1[0])
+# a = BSTNode("")
+# bst = BSTNode(names_1[0])
 
-for name in names_1:
-    a.insert(name)
+# for name in names_1:
+#     a.insert(name)
 
-for name2 in names_2:
-    if a.contains(name2):
-        duplicates.append(name2)
+# for name2 in names_2:
+#     if a.contains(name2):
+#         duplicates.append(name2)
 
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
 
+# -- BEST SOLUTION--, O(1)
+# 0.01sec runtime
+# make dictionary because constant time
+names_table = {}
+# add all names in first list to dictionary
+for name in names_1:
+    names_table[name] = True
+# check if names in dictionary are in second list
+for name in names_2:
+    if name in names_table:
+        duplicates.append(name)
+
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
